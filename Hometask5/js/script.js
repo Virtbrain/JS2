e3091eb7a282f1075350a23fd3863aa5f942e261
Alexey <virtbrain@gmail.com>
2018-10-18 01:28:56 +0500

    part1

---
+++ Hometask5/js/script.js
@@ -0,0 +1,37 @@
+const products = [
+    {
+        id: -1,
+        name: "Не выбрано",
+        price: 0
+    },
+    {
+        id: 0,
+        name: "iPhone X",
+        price: 75000
+    },
+    {
+        id: 1,
+        name: "iPad",
+        price: 35000
+    },
+    {
+        id: 2,
+        name: "Macbook Air",
+        price: 60000
+    },
+    {
+        id: 3,
+        name: "Macbook Pro",
+        price: 135000
+    }
+]
+
+const addListeners = () => {
+
+}
+
+const init = () => {
+    addListeners();
+}
+
+document.addEventListener("DOMContentLoaded", init);
